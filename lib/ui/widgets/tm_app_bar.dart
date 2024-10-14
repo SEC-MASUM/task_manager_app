b87dbf97165cb7ad9570ab3a731c379364adc068
SEC-MASUM <masum.jess42@gmail.com>
2024-10-14 14:57:18 +0600

    create Profile Update Screen, And Add tag to navigate Update Screen in TMAppBar

--- lib/ui/widgets/tm_app_bar.dart
+++ lib/ui/widgets/tm_app_bar.dart
@@ -1,55 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/ui/screens/profile_screen.dart';
 import 'package:task_manager_app/ui/screens/sign_in_screen.dart';
 import 'package:task_manager_app/ui/utils/app_colors.dart';
 
 class TMAppBar extends StatelessWidget implements PreferredSizeWidget {
   const TMAppBar({
     super.key,
+    this.isProfileScreenOpen = false,
   });
+
+  final bool isProfileScreenOpen;
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: AppColors.themeColor,
-      title: Row(
-        children: [
-          const CircleAvatar(
-            radius: 16,
-            backgroundColor: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        if (isProfileScreenOpen) {
+          return;
+        }
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProfileScreen(),
           ),
-          const SizedBox(width: 16),
-          const Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Masum Billah",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "masum.jess42@gmail.com",
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+        );
+      },
+      child: AppBar(
+        backgroundColor: AppColors.themeColor,
+        title: Row(
+          children: [
+            const CircleAvatar(
+              radius: 16,
+              backgroundColor: Colors.white,
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()),
-                  (predicate) => false);
-            },
-            icon: const Icon(Icons.logout),
-          )
-        ],
+            const SizedBox(width: 16),
+            const Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Masum Billah",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    "masum.jess42@gmail.com",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignInScreen()),
+                    (predicate) => false);
+              },
+              icon: const Icon(Icons.logout),
+            )
+          ],
+        ),
       ),
     );
   }
