7b9de145d7cad00d2a2cf6d887118b3e0678824a
SEC-MASUM <masum.jess42@gmail.com>
2024-11-06 23:56:03 +0600

    add newTaskList url

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,7 +1,8 @@
 class Urls {
-  static const String _baseUrl = "http://152.42.163.176:2006/api/v1";
+  static const String _baseUrl = "http://35.73.30.144:2005/api/v1";
 
   static const String registration = "$_baseUrl/registration";
   static const String login = "$_baseUrl/login";
   static const String addNewTask = "$_baseUrl/createTask";
+  static const String newTaskList = "$_baseUrl/listTaskByStatus/New";
 }
